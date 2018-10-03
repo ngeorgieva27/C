01cfc6b9c7bc9850ffe392ba3e5666edd954056f
ngeorgieva27 <43601148+ngeorgieva27@users.noreply.github.com>
2018-10-03 11:31:38 +0300

    kachen dialog
    kachen fail

---
+++ dialog.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Kak se kazvash?";
+    char ime[15];
+    cin>>ime;
+    cout<<"Zdravei" <<ime<<"!"<<endl;
+    cout<<"Na kolko si godini?";
+    char ime[4];
+    cin>>godini;
+    return 0;
+}
